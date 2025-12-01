d9e55afe07a1c18e5314de773adb9520b1004693
elkin <tavo07022@gmail.com>
2025-12-01 05:35:59 -0500

    commit inicial

---
+++ src/holamundo.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main(){
+
+    /*   int x; // Declaración
+         x = 5; // Asignación
+    */
+     int x = 5;
+     int Y = 6;
+     int suma = x + Y;
+
+     std::cout<<suma;
+
+    return 0;
+}
+
+
+
